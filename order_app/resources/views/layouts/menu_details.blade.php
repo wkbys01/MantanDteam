7b223cea45f3c46a2f241d376c6acbfdc4cb42c0
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-06 13:38:05 +0900

    基本形

--- order_app/resources/views/layouts/menu_details.blade.php
+++ order_app/resources/views/layouts/menu_details.blade.php
@@ -21,17 +21,58 @@
                 <button class="x_btn" id="xBtn">×</button>
             </div>
 
+            <div class="main_container">
+                <div class="photo_container">
+                    {{-- 仮置き --}}
+                    <img src="images/top_page/top_recommend_limited.jpg" alt="メニューの写真">
+                    <ul class="allergy_contents">
+                        <li>
+                            <img src="{{ asset('images/icon/allergy_crab')}}" alt="かに">
+                        </li>
+                        <li>
+                            <img src="images/icon/allergy_egg" alt="卵">
+                        </li>
+                        <li>
+                            <img src="images/icon/allergy_milk" alt="乳">
+                        </li>
+                        <li>
+                            <img src="images/icon/allergy_shrimp" alt="えび">
+                        </li>
+                        <li>
+                            <img src="images/icon/allergy_soba" alt="そば">
+                        </li>
+                        <li>
+                            <img src="images/icon/allergy_wheat" alt="小麦">
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="option_container">
+                    <ul class="option_contens">
+                        <li class="option_content">
+                            <h3 class="option_name">グラム変更</h3>
+                            <div class="option_btn_box">
+                                <button>小<span class="option_price">+xxx円</span></button>
+                                <button>小<span class="option_price">+xxx円</span></button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
             <div class="footer_container">
                 <div class="total_price_box">
                     <p>合計金額：</p>
                     <p class="total_price">？？？？</p>
                 </div>
-                <div class="quantity_adjust_box">
-                    <button class="minus_btn"></button>
-                    <p class="quantity">1</p>
-                    <button class="plus_btn"></button>
+                <div class="btn_container">
+                    <div class="quantity_adjust_box">
+                        <button class="minus_btn"></button>
+                        <p class="quantity">1</p>
+                        <button class="plus_btn"></button>
+                    </div>
+                    <button class="add_btn" id="addBtn">追加</button>
                 </div>
-                <button class="add_btn" id="addBtn">追加</button>
             </div>
 
         </div>
