003f470fefeefbf05875d4c882003d78c20ea617
asano <asano.s.24kdgn@gmail.com>
2025-09-08 01:48:29 +0900

    history_back_btn_fix

---
+++ order_app/app/Http/Controllers/HistoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class HistoryController extends Controller
+{
+    // 作ったけど使わないかも
+}
