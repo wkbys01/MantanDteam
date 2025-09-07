6f976137e44b3cfec77fd6874b5416adf9da6877
kano <yupakku@icloud.com>
2025-09-08 08:08:27 +0900

    サイドバーの修正

--- order_app/resources/views/layouts/side_bar_sab.blade.php
+++ order_app/resources/views/layouts/side_bar_sab.blade.php
@@ -9,47 +9,53 @@
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=Zen+Maru+Gothic:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/side_bar_sab.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/side_bar.css') }}">
 </head>
 <body>
 
     <header class="side_bar_container">
 
-        <!-- jsで選択時のスタイルを実装予定 -->
         <div class="language_container">
             <div class="language_box">
-                <button type="button" class="japan_box icon_flex_btn">
-                    <img src="images/icon/flag_japan.svg" alt="日本国旗">
-                    <p class="ja_txt langage_txt">日本語</p>
+                <button class="japan_box icon_flex_btn" id="jaBtn">
+                    <img src="{{ asset('images/icon/flag_japan.svg') }}" alt="日本国旗">
+                    <p class="ja_txt langage_txt" id="jaTxt">日本語</p>
                 </button>
-                <button type="button" class="china_box icon_flex_btn">
-                    <img src="images/icon/flag_china.svg" alt="中国国旗">
-                    <p class="cn_txt langage_txt">中文</p>
+                <button class="china_box icon_flex_btn" id="cnBtn">
+                    <img src="{{ asset('images/icon/flag_china.svg') }}" alt="中国国旗">
+                    <p class="cn_txt langage_txt" id="cnTxt">中文</p>
                 </button>
-                <button type="button" class="japan_box icon_flex_btn">
-                    <img src="images/icon/flag_japan.svg" alt="英語圏国旗">
-                    <p class="en_txt langage_txt">English</p>
+                <button class="japan_box icon_flex_btn" id="enBtn">
+                    <img src="{{ asset('images/icon/flag.svg') }}" alt="英語圏国旗">
+                    <p class="en_txt langage_txt" id="enTxt">English</p>
                 </button>
             </div>
         </div>
 
-
         <div class="btn_list_container">
-            <div class="call_btn popup_btn h_50 round">
-                <button type="button" class="icon_flex_btn">
-                    <img src="images/icon/icon_bell01.svg" alt="ベル" class="icon_img">
-                    <p>呼び出し</p>
-                </button>
-            </div>
-            <div class="checkout_btn popup_btn h_65 round">
-                <button type="button" class="icon_flex_btn">
-                    <img src="images/icon/icon_checkout01.svg" alt="お金" class="icon_img">
-                    <p>お会計</p>
-                </button>
+            <div class="btn_list">
+                <button class="icon_flex_btn" id="listBtn"></button>
+                <button class="icon_flex_btn" id="orderBtn"></button>
+                <a href="#" class="icon_flex_btn" id="historyBtn"></a>
+                <div class="call_btn popup_btn h_50 round">
+                    <button class="icon_flex_btn" id="callBtn">
+                        <img src="{{ asset('images/icon/icon_bell01.svg') }}" alt="ベル" class="icon_img">
+                        <p>呼び出し</p>
+                    </button>
+                </div>
+                <div class="checkout_btn popup_btn h_65 round">
+                    <button class="icon_flex_btn" id="checkoutBtn">
+                        <img src="{{ asset('images/icon/icon_checkout01.svg') }}" alt="お金" class="icon_img">
+                        <p>お会計</p>
+                    </button>
+                </div>
             </div>
         </div>
 
     </header>
 
+
+    @include('layouts.dialog')
+    <script src="{{ asset('js/side_bar.js') }}"></script>
 </body>
 </html>
