75d4c443265d964ee9851f6f4559b1016e3a86db
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-09 11:42:04 +0900

    topのbladeを共通化用に編集

--- order_app/resources/views/menus/test.blade.php
+++ order_app/resources/views/menus/test.blade.php
@@ -1,20 +1,82 @@
 @extends('layouts.app')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/decision.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/top_page.css') }}">
 @endpush
 
 
 @section('content')
 
+@include('layouts.side_bar')
+
 <main>
+<!-- 区切り線は擬似要素に変更する予定 -->
+<div class="top_page_container">
+@foreach($main_categories as $index => $category)
+    @php
+        $locale = app()->getLocale(); // 現在の言語
+        $texts = $category->translations[$locale] ?? $category->translations['ja'];
+    @endphp
 
-    <h1 class="notice_txt">ご利用ありがとうございました。</h1>
-    <p class="guide_txt">レシートを持ってレジまでお越しください。</p>
-    <div class="people_img checkout_img">
-        <img src="images/people/people02.png" alt="案内する人">
-    </div>
-
+        @if($index === 0)
+            <div class="recommend_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                    <div class="spacer">{{ $texts[0] }}</div>
+                    <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @elseif($index === 1)
+            <div class="grand_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 2)
+            <div class="set_meal_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 3)
+            <div class="set_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 4)
+            <div class="child_single_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                        <div>{{ $texts[0] }}</div>
+                        <img src="images/top_page/top_line_stroke.png" alt="#">
+                        <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @elseif($index === 5)
+            <div class="take_out_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                <div class="btn_txt">
+                    <div class="spacer">{{ mb_substr($texts, 0, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
+                    <div>{{ mb_substr($texts, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
+                </div>
+                </a>
+            </div>
+        @elseif($index === 6)
+            <div class="drink_dessert_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                        <div>{{ $texts[0] }}</div>
+                        <img src="images/top_page/top_line_stroke.png" alt="#">
+                        <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @endif
+    @endforeach
+</div>
 </main>
 
 
--- order_app/resources/views/menus/top.blade.php
+++ order_app/resources/views/menus/top.blade.php
@@ -1,88 +1,83 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>マンタン</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=Zen+Maru+Gothic:wght@400;500;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/top_page.css') }}">
-</head>
-<body>
-    @include('layouts.side_bar')
-    {{-- @include('layouts.dialog') --}}
+@extends('layouts.app')
 
-  <main>
-    <!-- 区切り線は擬似要素に変更する予定 -->
-    <div class="top_page_container">
-      @foreach($main_categories as $index => $category)
-          @php
-            $locale = app()->getLocale(); // 現在の言語
-            $texts = $category->translations[$locale] ?? $category->translations['ja']; 
-          @endphp
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/top_page.css') }}">
+@endpush
 
-            @if($index === 0)
-                <div class="recommend_btn link_btn">
-                    <a href="{{ route('menu_page', $category->id) }}">
-                        <div class="btn_txt">
-                          <div class="spacer">{{ $texts[0] }}</div>
-                          <div>{{ $texts[1] }}</div>
-                        </div>
-                    </a>
+
+@section('content')
+
+@include('layouts.side_bar')
+
+<main>
+<!-- 区切り線は擬似要素に変更する予定 -->
+<div class="top_page_container">
+@foreach($main_categories as $index => $category)
+    @php
+        $locale = app()->getLocale(); // 現在の言語
+        $texts = $category->translations[$locale] ?? $category->translations['ja'];
+    @endphp
+
+        @if($index === 0)
+            <div class="recommend_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                    <div class="spacer">{{ $texts[0] }}</div>
+                    <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @elseif($index === 1)
+            <div class="grand_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 2)
+            <div class="set_meal_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 3)
+            <div class="set_btn link_btn">
+            <a href="{{ route('menu_page', $category->id) }}">
+                <p class="btn_txt">{{ $texts }}</p>
+            </a>
+            </div>
+        @elseif($index === 4)
+            <div class="child_single_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                        <div>{{ $texts[0] }}</div>
+                        <img src="images/top_page/top_line_stroke.png" alt="#">
+                        <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @elseif($index === 5)
+            <div class="take_out_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                <div class="btn_txt">
+                    <div class="spacer">{{ mb_substr($texts, 0, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
+                    <div>{{ mb_substr($texts, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
                 </div>
-            @elseif($index === 1)
-                <div class="grand_btn link_btn">
-                  <a href="{{ route('menu_page', $category->id) }}">
-                    <p class="btn_txt">{{ $texts }}</p>
-                  </a>
-                </div>
-            @elseif($index === 2)
-                <div class="set_meal_btn link_btn">
-                  <a href="{{ route('menu_page', $category->id) }}">
-                    <p class="btn_txt">{{ $texts }}</p>
-                  </a>
-                </div>
-            @elseif($index === 3)
-                <div class="set_btn link_btn">
-                  <a href="{{ route('menu_page', $category->id) }}">
-                    <p class="btn_txt">{{ $texts }}</p>
-                  </a>
-                </div>
-            @elseif($index === 4)
-                <div class="child_single_btn link_btn">
-                    <a href="{{ route('menu_page', $category->id) }}">
-                        <div class="btn_txt">
-                            <div>{{ $texts[0] }}</div>
-                            <img src="images/top_page/top_line_stroke.png" alt="#">
-                            <div>{{ $texts[1] }}</div>
-                        </div>
-                    </a>
-                </div>
-            @elseif($index === 5)
-                <div class="take_out_btn link_btn">
-                    <a href="{{ route('menu_page', $category->id) }}">
-                      <div class="btn_txt">
-                          <div class="spacer">{{ mb_substr($texts, 0, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
-                          <div>{{ mb_substr($texts, app()->getLocale() === 'ja' ? 3 : 4) }}</div>
-                      </div>
-                    </a>
-                </div>
-            @elseif($index === 6)
-                <div class="drink_dessert_btn link_btn">
-                    <a href="{{ route('menu_page', $category->id) }}">
-                        <div class="btn_txt">
-                            <div>{{ $texts[0] }}</div>
-                            <img src="images/top_page/top_line_stroke.png" alt="#">
-                            <div>{{ $texts[1] }}</div>
-                        </div>
-                    </a>
-                </div>
-            @endif
-        @endforeach
-    </div>
-  </main>
-</body>
-</html>
+                </a>
+            </div>
+        @elseif($index === 6)
+            <div class="drink_dessert_btn link_btn">
+                <a href="{{ route('menu_page', $category->id) }}">
+                    <div class="btn_txt">
+                        <div>{{ $texts[0] }}</div>
+                        <img src="images/top_page/top_line_stroke.png" alt="#">
+                        <div>{{ $texts[1] }}</div>
+                    </div>
+                </a>
+            </div>
+        @endif
+    @endforeach
+</div>
+</main>
+
+
+@endsection
