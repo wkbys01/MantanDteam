7a75bb42efa9c4ba30f7bbdb0d9d53b3513c4d8f
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-09 12:10:03 +0900

    side_barのbladeを共通化用に編集

--- order_app/resources/views/layouts/side_bar.blade.php
+++ order_app/resources/views/layouts/side_bar.blade.php
@@ -1,98 +1,93 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>マンタン</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=Zen+Maru+Gothic:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+@extends('layouts.app')
+
+@push('styles')
     <link rel="stylesheet" href="{{ asset('css/side_bar.css') }}">
-</head>
-<body>
+@endpush
 
-    <header class="side_bar_container">
 
-        <div class="language_container">
-            <div class="language_box">
-                <button class="japan_box icon_flex_btn" id="jaBtn">
-                    <img src="{{ asset('images/icon/flag_japan.svg') }}" alt="日本国旗">
-                    <p class="ja_txt langage_txt" id="jaTxt">日本語</p>
+@section('content')
+
+<header class="side_bar_container">
+
+    <div class="language_container">
+        <div class="language_box">
+            <button class="japan_box icon_flex_btn" id="jaBtn">
+                <img src="{{ asset('images/icon/flag_japan.svg') }}" alt="日本国旗">
+                <p class="ja_txt langage_txt" id="jaTxt">日本語</p>
+            </button>
+            <button class="china_box icon_flex_btn" id="cnBtn">
+                <img src="{{ asset('images/icon/flag_china.svg') }}" alt="中国国旗">
+                <p class="cn_txt langage_txt" id="cnTxt">中文</p>
+            </button>
+            <button class="japan_box icon_flex_btn" id="enBtn">
+                <img src="{{ asset('images/icon/flag.svg') }}" alt="英語圏国旗">
+                <p class="en_txt langage_txt" id="enTxt">English</p>
+            </button>
+        </div>
+    </div>
+
+
+    <div class="btn_list_container">
+        <div class="btn_list">
+            <div class="list_btn popup_btn h_50 round">
+                <button class="icon_flex_btn" id="listBtn">
+                    <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
+                    <p>注文リスト</p>
                 </button>
-                <button class="china_box icon_flex_btn" id="cnBtn">
-                    <img src="{{ asset('images/icon/flag_china.svg') }}" alt="中国国旗">
-                    <p class="cn_txt langage_txt" id="cnTxt">中文</p>
+            </div>
+            <div class="order_list_box round">
+                <ul class="order_list">
+                    <!-- テスト用 -->
+                    <div class="order_separator">
+                        <li class="order_menu_txt">満腹まんたんセット</li>
+                        <li class="order_option_txt">トッピング</li>
+                        <li class="order_option_txt">ドリンクバー</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">たっぷりチーズのドリア</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">ふわとろオムライス</li>
+                        <li class="order_option_txt">デミグラス</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">まんたんハンバーグ定食</li>
+                    </div>
+                </ul>
+            </div>
+            <div class="order_btn popup_btn h_65 round">
+                <button class="icon_flex_btn" id="orderBtn">
+                    <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
+                    <p>注文確定</p>
                 </button>
-                <button class="japan_box icon_flex_btn" id="enBtn">
-                    <img src="{{ asset('images/icon/flag.svg') }}" alt="英語圏国旗">
-                    <p class="en_txt langage_txt" id="enTxt">English</p>
+            </div>
+            <div class="history_btn page_btn h_50 round">
+                <a href="/history_page" class="icon_flex_btn" id="historyBtn">
+                    <img src="{{ asset('images/icon/icon_history.svg') }}" alt="履歴" class="icon_img">
+                    注文履歴
+                </a>
+            </div>
+            <div class="call_btn popup_btn h_50 round">
+                <button class="icon_flex_btn" id="callBtn">
+                    <img src="{{ asset('images/icon/icon_bell01.svg') }}" alt="ベル" class="icon_img">
+                    <p>呼び出し</p>
+                </button>
+            </div>
+            <div class="checkout_btn popup_btn h_65 round">
+                <button class="icon_flex_btn" id="checkoutBtn">
+                    <img src="{{ asset('images/icon/icon_checkout01.svg') }}" alt="お金" class="icon_img">
+                    <p>お会計</p>
                 </button>
             </div>
         </div>
+    </div>
+
+</header>
 
 
-        <div class="btn_list_container">
-            <div class="btn_list">
-                <div class="list_btn popup_btn h_50 round">
-                    <button class="icon_flex_btn" id="listBtn">
-                        <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
-                        <p>注文リスト</p>
-                    </button>
-                </div>
-                <div class="order_list_box round">
-                    <ul class="order_list">
-                        <!-- テスト用 -->
-                        <div class="order_separator">
-                            <li class="order_menu_txt">満腹まんたんセット</li>
-                            <li class="order_option_txt">トッピング</li>
-                            <li class="order_option_txt">ドリンクバー</li>
-                        </div>
-                        <div class="order_separator">
-                            <li class="order_menu_txt">たっぷりチーズのドリア</li>
-                        </div>
-                        <div class="order_separator">
-                            <li class="order_menu_txt">ふわとろオムライス</li>
-                            <li class="order_option_txt">デミグラス</li>
-                        </div>
-                        <div class="order_separator">
-                            <li class="order_menu_txt">まんたんハンバーグ定食</li>
-                        </div>
-                    </ul>
-                </div>
-                <div class="order_btn popup_btn h_65 round">
-                    <button class="icon_flex_btn" id="orderBtn">
-                        <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
-                        <p>注文確定</p>
-                    </button>
-                </div>
-                <div class="history_btn page_btn h_50 round">
-                    <a href="/history_page" class="icon_flex_btn" id="historyBtn">
-                        <img src="{{ asset('images/icon/icon_history.svg') }}" alt="履歴" class="icon_img">
-                        注文履歴
-                    </a>
-                </div>
-                <div class="call_btn popup_btn h_50 round">
-                    <button class="icon_flex_btn" id="callBtn">
-                        <img src="{{ asset('images/icon/icon_bell01.svg') }}" alt="ベル" class="icon_img">
-                        <p>呼び出し</p>
-                    </button>
-                </div>
-                <div class="checkout_btn popup_btn h_65 round">
-                    <button class="icon_flex_btn" id="checkoutBtn">
-                        <img src="{{ asset('images/icon/icon_checkout01.svg') }}" alt="お金" class="icon_img">
-                        <p>お会計</p>
-                    </button>
-                </div>
-            </div>
-        </div>
-
-    </header>
+@include('layouts.dialog')
+@include('layouts.list_details')
+<script src="{{ asset('js/side_bar.js') }}"></script>
 
 
-    @include('layouts.dialog')
-    @include('layouts.list_details')
-    <script src="{{ asset('js/side_bar.js') }}"></script>
-</body>
-</html>
+@endsection
--- order_app/resources/views/menus/test.blade.php
+++ order_app/resources/views/menus/test.blade.php
@@ -1,132 +1,93 @@
 @extends('layouts.app')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/history_page.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/side_bar.css') }}">
 @endpush
 
 
 @section('content')
 
-<div class="wapper">
-    <div class="main-content">
-        <div class="header_div">
-            <h1>注文履歴</h1>
-            <a href="{{ url()->previous() }}" class="header_button">メニューに戻る</a>
-        </div>
-        <main>
-            <table>
-                <thead>
-                    <tr>
-                        <th>商品名</th>
-                        <th>単価</th>
-                        <th>数量</th>
-                        <th>ご提供</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="main_td">お子様うどん</td>
-                        <td>600円</td>
-                        <td>2</td>
-                        <td>○</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:冷たい</td>
-                        <td>0円</td>
-                        <td>2</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:ドリンクバー</td>
-                        <td>100円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td">きつねうどん</td>
-                        <td>700円</td>
-                        <td>1</td>
-                        <td>×</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:温かい</td>
-                        <td>0円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td">お子様うどん</td>
-                        <td>600円</td>
-                        <td>2</td>
-                        <td>○</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:冷たい</td>
-                        <td>0円</td>
-                        <td>2</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:ドリンクバー</td>
-                        <td>100円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td">きつねうどん</td>
-                        <td>700円</td>
-                        <td>1</td>
-                        <td>×</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:温かい</td>
-                        <td>0円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td">お子様うどん</td>
-                        <td>600円</td>
-                        <td>2</td>
-                        <td>○</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:冷たい</td>
-                        <td>0円</td>
-                        <td>2</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:ドリンクバー</td>
-                        <td>100円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td class="main_td">きつねうどん</td>
-                        <td>700円</td>
-                        <td>1</td>
-                        <td>×</td>
-                    </tr>
-                    <tr>
-                        <td class="main_td_sab">:温かい</td>
-                        <td>0円</td>
-                        <td>1</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </main>
-        <div class="footer_div">
-            <h2>合計金額</h2>
-            <p class="footer_p">:</p>
-            <p>6000円</p>
+<header class="side_bar_container">
+
+    <div class="language_container">
+        <div class="language_box">
+            <button class="japan_box icon_flex_btn" id="jaBtn">
+                <img src="{{ asset('images/icon/flag_japan.svg') }}" alt="日本国旗">
+                <p class="ja_txt langage_txt" id="jaTxt">日本語</p>
+            </button>
+            <button class="china_box icon_flex_btn" id="cnBtn">
+                <img src="{{ asset('images/icon/flag_china.svg') }}" alt="中国国旗">
+                <p class="cn_txt langage_txt" id="cnTxt">中文</p>
+            </button>
+            <button class="japan_box icon_flex_btn" id="enBtn">
+                <img src="{{ asset('images/icon/flag.svg') }}" alt="英語圏国旗">
+                <p class="en_txt langage_txt" id="enTxt">English</p>
+            </button>
         </div>
     </div>
-    <div>
-        @include('layouts.side_bar_sab')
+
+
+    <div class="btn_list_container">
+        <div class="btn_list">
+            <div class="list_btn popup_btn h_50 round">
+                <button class="icon_flex_btn" id="listBtn">
+                    <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
+                    <p>注文リスト</p>
+                </button>
+            </div>
+            <div class="order_list_box round">
+                <ul class="order_list">
+                    <!-- テスト用 -->
+                    <div class="order_separator">
+                        <li class="order_menu_txt">満腹まんたんセット</li>
+                        <li class="order_option_txt">トッピング</li>
+                        <li class="order_option_txt">ドリンクバー</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">たっぷりチーズのドリア</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">ふわとろオムライス</li>
+                        <li class="order_option_txt">デミグラス</li>
+                    </div>
+                    <div class="order_separator">
+                        <li class="order_menu_txt">まんたんハンバーグ定食</li>
+                    </div>
+                </ul>
+            </div>
+            <div class="order_btn popup_btn h_65 round">
+                <button class="icon_flex_btn" id="orderBtn">
+                    <img src="{{ asset('images/icon/icon_cart01.svg') }}" alt="ショッピングカート" class="icon_img">
+                    <p>注文確定</p>
+                </button>
+            </div>
+            <div class="history_btn page_btn h_50 round">
+                <a href="/history_page" class="icon_flex_btn" id="historyBtn">
+                    <img src="{{ asset('images/icon/icon_history.svg') }}" alt="履歴" class="icon_img">
+                    注文履歴
+                </a>
+            </div>
+            <div class="call_btn popup_btn h_50 round">
+                <button class="icon_flex_btn" id="callBtn">
+                    <img src="{{ asset('images/icon/icon_bell01.svg') }}" alt="ベル" class="icon_img">
+                    <p>呼び出し</p>
+                </button>
+            </div>
+            <div class="checkout_btn popup_btn h_65 round">
+                <button class="icon_flex_btn" id="checkoutBtn">
+                    <img src="{{ asset('images/icon/icon_checkout01.svg') }}" alt="お金" class="icon_img">
+                    <p>お会計</p>
+                </button>
+            </div>
+        </div>
     </div>
-</div>
+
+</header>
+
+
+@include('layouts.dialog')
+@include('layouts.list_details')
+<script src="{{ asset('js/side_bar.js') }}"></script>
 
 
 @endsection
