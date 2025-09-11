e6c651750fc3a592d8e637938eb25004cca3ab09
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-12 02:40:54 +0900

    menuページでの注文リスト表示修正

--- order_app/resources/views/layouts/menu_details.blade.php
+++ order_app/resources/views/layouts/menu_details.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
+    {{-- <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>マンタン</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=Zen+Maru+Gothic:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+Rounded+1c&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=Zen+Maru+Gothic:wght@400;500;700&display=swap" rel="stylesheet"> --}}
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('css/menu_details.css') }}">
 </head>
 <body>
@@ -26,7 +26,7 @@
                     {{-- 仮置き --}}
                     <img src="" alt="メニューの写真" class="menu_photo" id="modalImage">
                     <ul class="allergy_contents"><!-- jsで差し替え --></ul>
-                    
+
                 </div>
 
                 <div class="option_container">
