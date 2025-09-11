6c48fbd7ae84555fbcef2ea617f2335aa4f214cd
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-11 19:36:26 +0900

    css変更

---
+++ order_app/public/js/decision.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const callPage = document.getElementsByClassName("call_container");
+//     const orderPage = document.getElementsByClassName("order_container");
+//     const checkoutPage = document.getElementsByClassName("checkout_container");
+
+//     callPage.addEventListener("click", () => {
+//         history.back();
+//     })
+// });
