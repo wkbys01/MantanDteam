0c6a8a1807d1f12a675082533db6e34411a95336
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-09-12 15:10:57 +0900

    確定後ページからのページ遷移の時間を3秒に変更

--- order_app/public/js/decision.js
+++ order_app/public/js/decision.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const path = window.location.pathname;
 
     if (path === "/checkout_page") {
-        // 5秒後に自動でtopページに戻る
+        // 3秒後に自動でtopページに戻る
         setTimeout(function() {
             window.location.href = "/start_page";
-        }, 5000);
+        }, 3000);
     } else {
         // クリックで前のページに戻る
         document.body.addEventListener("click", () => {
             window.history.back();
         });
 
-        // 5秒後に自動で前のページに戻る
+        // 3秒後に自動で前のページに戻る
         setTimeout(function() {
             window.history.back();
-        }, 5000);
+        }, 3000);
     }
 });
